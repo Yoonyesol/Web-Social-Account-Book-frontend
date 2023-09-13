2c665d771f67c5c890397dba05aab2e4531387c5
yesol <maniac970430@naver.com>
2023-09-13 23:24:44 +0900

    :fire: 불필요한 패키지 및 파일 삭제

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
-
-registerServiceWorker();

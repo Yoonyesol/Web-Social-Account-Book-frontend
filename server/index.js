1f2f5bf63d1b5cdf1842cf3c17017c4074f10e44
yesol <maniac970430@naver.com>
2023-09-13 23:09:04 +0900

    :recycle: 채팅 코드 리팩토링

--- server/index.js
+++ server/index.js
@@ -1,60 +1,25 @@
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-// const cors = require("cors");
-// const { Server } = require("socket.io");
-// app.use(cors());
-
-// const server = http.createServer();
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"]
-//   },
-// }); //cors오류 설정
-
-// io.on("connection", (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
-
-//   socket.on("join_room", (data) => {
-//     socket.join(data);
-//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-//   });
-
-//   socket.on("send_message", (data) => {
-//     socket.emit("receive_message", data);
-//     console.log(data)
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("User Disconnected", socket.id);
-//   });
-// });
-
-// server.listen(80, () => {
-//   console.log("SERVER RUNNING");
-// });
-
-const httpServer = require("http").createServer();
+const httpServer = require("http").createServer(); //서버 객체 생성
+// http server를 socket.io server로 upgrade
 const io = require("socket.io")(httpServer, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
-});
+}); //cors 정책 설정
 
+//socket 연결
 io.on("connection", (socket) => {
   console.log("connection");
   socket.on("init", (payload) => {
     console.log(payload);
   });
 
-  socket.on("send message", (item) => {//send message 이벤트 발생
-     console.log(item.author + " : " + item.message);
-    io.emit("receive message", { author: item.author, message: item.message, time:item.time });
+  //연결이 되면 클라이언트에 메시지를 전송하고 클라이언트로부터 메시지를 수신한다
+  socket.on("send message", (item) => {
+    console.log(item.author + " : " + item.message);
+    io.emit("receive message", { author: item.author, message: item.message, time: item.time });
     //클라이언트에 이벤트를 보냄
   });
 });
 
-httpServer.listen(80);
+httpServer.listen(80); //80번 포트 서버 실행

04446a4b8284d3908174300aca61f9c21f22887d
Yoonyesol <maniac970430@naver.com>
2024-02-04 22:37:25 +0900

    ✨feat: 회원가입 기능 구현
    - 예외처리 및 오류 표시는 아직 진행하지 않음

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,198 +1,169 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { useLocation } from "react-router-dom";
-import { GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL, GITHUB_AUTH_URL } from "../constants";
-import { Link, Navigate } from "react-router-dom";
-import fbLogo from "../assets/img/fb-logo.png";
-import googleLogo from "../assets/img/google-logo.png";
-import githubLogo from "../assets/img/github-logo.png";
+import Button from "../common/Button";
+import { signupAPI } from "../utils/userAPI";
+import { useNavigate } from "react-router-dom";
 
-export default function Auth({ userInfo }) {
-  let location = useLocation();
+export default function Auth() {
+  const nav = useNavigate();
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
-  if (userInfo.authenticated) {
-    return (
-      <Navigate
-        to={{
-          pathname: "/",
-          state: { from: location },
-        }}
-      />
-    );
-  }
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleAuthSubmit = async (e) => {
+    e.preventDefault();
+
+    if (isLoginMode) {
+    } else {
+      try {
+        await signupAPI(form);
+        alert("회원가입에 성공했습니다!");
+        nav("/");
+      } catch (err) {
+        console.log("API 호출 도중 에러 발생:", err.message);
+      }
+    }
+  };
+
+  const switchModeHandler = () => {
+    setIsLoginMode((prevMode) => !prevMode);
+  };
+
   return (
     <Section>
-      <div className="login-container">
-        <div className="login-content">
-          <h1 className="login-title">로그인</h1>
-          <div className="social-login">
-            <a className="btn btn-block social-btn google" href={GOOGLE_AUTH_URL}>
-              <img src={googleLogo} alt="Google" /> Log in with Google
-            </a>
-            <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
-              <img src={fbLogo} alt="Facebook" /> Log in with Facebook
-            </a>
-            <a className="btn btn-block social-btn github" href={GITHUB_AUTH_URL}>
-              <img src={githubLogo} alt="Github" /> Log in with Github
-            </a>
+      <div className="container">
+        <h1>{isLoginMode ? "로그인" : "회원가입"}</h1>
+        <form onSubmit={handleAuthSubmit}>
+          {!isLoginMode && (
+            <div className="form-field">
+              <h3>이름</h3>
+              <div className="form-item">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="이름"
+                  value={form.name}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+            </div>
+          )}
+          <div className="form-field">
+            <h3>이메일</h3>
+            <div className="form-item">
+              <input
+                type="email"
+                name="email"
+                placeholder="이메일"
+                value={form.email}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
           </div>
-          <div className="or-separator">
-            <span className="or-text">OR</span>
+          <div className="form-field">
+            <h3>비밀번호</h3>
+            <div className="form-item">
+              <input
+                type="password"
+                name="password"
+                placeholder="비밀번호"
+                value={form.password}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
           </div>
-          <LoginForm />
-          <span className="signup-link">
-            아직 회원가입하지 않으셨나요? <Link to="/signup">회원가입</Link>
-          </span>
+          <div className="submit-btn">
+            <Button type="submit" text={isLoginMode ? "로그인" : "회원가입"} />
+          </div>
+        </form>
+        <div className="switch-mode">
+          <span>{isLoginMode ? "아직 회원이 아니신가요?" : "이미 회원이신가요?"}</span>
+          <Button color="red" text={isLoginMode ? "회원가입" : "로그인"} onClick={switchModeHandler} />
         </div>
       </div>
     </Section>
   );
 }
 
-function LoginForm() {
-  const [userInfo, setUserInfo] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserInfo({
-      ...userInfo,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  return (
-    <Section>
-      <form onSubmit={handleSubmit}>
-        <div className="form-item">
-          <input
-            type="email"
-            name="email"
-            className="form-control"
-            placeholder="Email"
-            value={userInfo.email}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="form-item">
-          <input
-            type="password"
-            name="password"
-            className="form-control"
-            placeholder="Password"
-            value={userInfo.password}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="form-item">
-          <button type="submit" className="loginBtn">
-            Login
-          </button>
-        </div>
-      </form>
-    </Section>
-  );
-}
-
 const Section = styled.section`
-  .login-container {
-    text-align: center;
-  }
+  margin-left: 18vw;
+  padding: 2rem;
+  height: 100%;
 
-  .social-login {
+  .container {
+    width: 50%;
+    margin: 0 auto;
     display: flex;
     flex-direction: column;
-  }
-
-  .login-content {
+    gap: 1.5rem;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
     background: #fff;
     box-shadow: 0 1px 11px rgba(0, 0, 0, 0.27);
-    border-radius: 2px;
-    width: 500px;
-    display: inline-block;
-    margin-top: 30px;
-    vertical-align: middle;
-    position: relative;
+    border-radius: 10px;
     padding: 35px;
   }
 
-  .social-btn {
-    margin-bottom: 15px;
-    font-weight: 400;
-    font-size: 16px;
-    border-radius: 0.5rem;
-    padding: 1rem;
-    border: 1px solid grey;
+  form {
+    border-top: solid 1px #b8b6b6;
+    width: 100%;
   }
 
-  .social-btn img {
-    height: 32px;
-    float: left;
-    margin-top: 10px;
+  .form-field {
+    margin-top: 1.2rem;
   }
 
-  .social-btn.google {
-    margin-top: 7px;
-  }
-
-  .social-btn.facebook img {
-    height: 24px;
-    margin-left: 3px;
-  }
-
-  .social-btn.github img {
-    height: 24px;
-    margin-left: 3px;
-  }
-
-  .signup-link {
-    color: rgba(0, 0, 0, 0.65);
-    font-size: 14px;
-  }
-
-  .login-title {
-    font-size: 1.5em;
-    font-weight: 500;
-    margin-top: 0;
-    margin-bottom: 30px;
-    color: rgba(0, 0, 0, 0.65);
-  }
-
-  .form-control {
-    margin: 0px 0px;
+  .form-item {
+    margin-top: 0.7rem;
   }
 
   .form-item > input {
     font-size: 15px;
-    font-weight: 500;
-    width: 14rem;
+    width: 100%;
+    height: 6vh;
     font-family: "Gowun Batang", serif;
-    margin: 0.3rem 0rem 0.1rem 0rem;
   }
 
-  .loginBtn {
-    margin: 1rem 0;
-    background-color: #5d8de6;
-    padding: 0.2rem 1rem;
-    font-size: 1rem;
-    font-family: "Gowun Batang", serif;
-    color: white;
-    border-radius: 0.5rem;
-    border: 0;
-    outline: 0;
-    cursor: pointer;
+  .submit-btn {
+    margin: 2rem 0;
+  }
+
+  .switch-mode {
+    color: rgba(0, 0, 0, 0.65);
+    font-size: 14px;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    align-items: center;
+    text-align: center;
+  }
+
+  button {
+    width: 8rem;
   }
 
   @media screen and (min-width: 280px) and (max-width: 1080px) {
-    .login-content {
+    margin-left: 0;
+
+    .content {
       width: 300px;
     }
   }
---
+++ src/utils/userAPI.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const fetchUserDataAPI = async () => {
+  try {
+    const responseData = await axios.get(`http://localhost:5000/api/users`);
+    return responseData.data.users;
+  } catch (e) {
+    console.log("HTTP request 도중 에러 발생:", e.message);
+    throw e;
+  }
+};
+
+export const signupAPI = async (form) => {
+  try {
+    const user = {
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    };
+    await axios.post(`http://localhost:5000/api/users/signup`, user);
+  } catch (e) {
+    console.log("HTTP request 도중 에러 발생:", e.message);
+    throw e;
+  }
+};
